8c4c264abc7bb6ccbaa2a0dfe651fa4f2eee39b9
Vickii <200028pq@yse-c.net>
2021-10-15 13:59:57 +0900

    syukka

--- syukka.php
+++ syukka.php
@@ -10,37 +10,48 @@
 数値以外が入力されています：入力された値に数字以外の文字が含まれている
 */
 /*
- * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
+ * ①vの結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①の処理を行う */) {
-	//②セッションを開始する
+if (/* ①の処理を行う */ session_status() === PHP_SESSION_NONE) {
+    //②セッションを開始する
+    session_start();
+    
 }
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
+if (/* ③の処理を書く */!$_SESSION["login"]){
+    //④SESSIONの「error2」に「ログインしてください」と設定する。
+    $_SESSION["error2"]="ログインしてください";
+    //⑤ログイン画面へ遷移する。
+    header("Location: login.php");
+    exit();
 }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
-
 //⑦データベースで使用する文字コードを「UTF8」にする
+$pdo = new PDO("mysql:host=localhost;dbname=zaiko2021_yse;charset=utf8;","zaiko2021_yse", "2021zaiko" );
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
+
+if(/* ⑧の処理を行う */empty($_POST["books"])){
+    //⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
+    $_SESSION["success"]="出荷する商品が選択されていません";
+    //⑩在庫一覧画面へ遷移する。
+    header("Location: zaiko_ichiran.php");
+    exit();
 }
 
 function getId($id,$con){
-	/* 
-	 * ⑪書籍を取得するSQLを作成する実行する。
-	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
-	 * SQLの実行結果を変数に保存する。
-	 */
-
-	//⑫実行した結果から1レコード取得し、returnで値を返す。
+    /* 
+     * ⑪書籍を取得するSQLを作成する実行する。
+     * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
+     * SQLの実行結果を変数に保存する。
+     */
+    $st = $con->query("SELECT * FROM books where id ={$id}");
+    $row = $st->fetch(PDO::FETCH_ASSOC);
+    //⑫実行した結果から1レコード取得し、returnで値を返す。
+    return $row;
 }
 ?>
 <!DOCTYPE html>
@@ -53,73 +64,77 @@ function getId($id,$con){
 <body>
 <!-- ヘッダ -->
 <div id="header">
-	<h1>出荷</h1>
+    <h1>出荷</h1>
 </div>
 
 <!-- メニュー -->
 <div id="menu">
-	<nav>
-		<ul>
-			<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
-		</ul>
-	</nav>
+    <nav>
+        <ul>
+            <li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
+        </ul>
+    </nav>
 </div>
 
 <form action="syukka_kakunin.php" method="post">
-	<div id="pagebody">
-		<!-- エラーメッセージ -->
-		<div id="error">
-		<?php
-		/*
-		 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
-		 * 設定されていた場合はif文の中に入る。
-		 */ 
-		if(/* ⑬の処理を書く */){
-			//⑭SESSIONの「error」の中身を表示する。
-		}
-		?>
-		</div>
-		<div id="center">
-			<table>
-				<thead>
-					<tr>
-						<th id="id">ID</th>
-						<th id="book_name">書籍名</th>
-						<th id="author">著者名</th>
-						<th id="salesDate">発売日</th>
-						<th id="itemPrice">金額(円)</th>
-						<th id="stock">在庫数</th>
-						<th id="in">出荷数</th>
-					</tr>
-				</thead>
-				<?php 
-				/*
-				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
-				 */
-				foreach(/* ⑮の処理を書く */){
-					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-				?>
-				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
-				<tr>
-					<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
-					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
-				</tr>
-				<?php
-				}
-				?>
-			</table>
-			<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
-		</div>
-	</div>
+    <div id="pagebody">
+        <!-- エラーメッセージ -->
+        <div id="error">
+        <?php
+        /*
+         * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
+         * 設定されていた場合はif文の中に入る。
+         */ 
+        if(@$_SESSION["error"]){
+            //⑭SESSIONの「error」の中身を表示する。
+            echo $_SESSION["error"];
+        }
+        ?>
+        </div>
+        <div id="center">
+            <table>
+                <thead>
+                    <tr>
+                        <th id="id">ID</th>
+                        <th id="book_name">書籍名</th>
+                        <th id="author">著者名</th>
+                        <th id="salesDate">発売日</th>
+                        <th id="itemPrice">金額(円)</th>
+                        <th id="itemPrice">金額(円)</th>
+                        <th id="itemPrice">金額(円)</th>
+                        <th id="stock">在庫数</th>
+                        <th id="in">出荷数</th>
+                    </tr>
+                </thead>
+                <?php 
+                /*
+                 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
+                 */
+                foreach($_POST['books'] as $book_id/* ⑮の処理を書く */){
+                    // ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
+                    $book =getId($book_id,$pdo);
+                    return $book;
+                ?>
+                <input type="hidden" value="<?php $book['id']   /* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
+                <tr>
+                    <td><?php $book['name'];    /* ⑱ ⑯の戻り値からidを取り出し、表示する */?></td>
+                    <td><?php $book['title'];   /* ⑲ ⑯の戻り値からtitleを取り出し、表示する */?></td>
+                    <td><?php $book['author'];  /* ⑳ ⑯の戻り値からauthorを取り出し、表示する */?></td>
+                    <td><?php $book['salesDate'];   /* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */?></td>
+                    <td><?php $book['price'];   /* ㉒ ⑯の戻り値からpriceを取り出し、表示する */?></td>
+                    <td><?php $book['stock'];   /* ㉓ ⑯の戻り値からstockを取り出し、表示する */?></td>
+                </tr>
+                <?php
+                }
+                ?>
+            </table>
+            <button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
+        </div>
+    </div>
 </form>
 <!-- フッター -->
 <div id="footer">
-	<footer>株式会社アクロイト</footer>
+    <footer>株式会社アクロイト</footer>
 </div>
 </body>
 </html>
